8eacb8778096bcecebeeee8ce31e0fbd67e9fe70
vinesaucebeep <88556555+vinesaucebeep@users.noreply.github.com>
2021-09-03 17:33:01 +0200

    Add files via upload

---
+++ src/main/java/org/neocities/beep67site/endupdate/common/properties/blocks/EnderCampfireBlock.java
@@ -0,0 +1,14 @@
+package org.neocities.beep67site.endupdate.common.properties.blocks;
+
+import net.minecraft.block.CampfireBlock;
+
+public class EnderCampfireBlock extends CampfireBlock {
+
+    public EnderCampfireBlock(boolean p_i241174_1_, int p_i241174_2_, Properties p_i241174_3_) {
+        super(p_i241174_1_, p_i241174_2_, p_i241174_3_);
+    }
+
+    public EnderCampfireBlock(Properties properties) {
+        super(false, 3, properties);
+    }
+}
